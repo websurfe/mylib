a39f65b7b33ff6c399be9f3efa416c8b3f2e56c9
wen <wen-git@gmail.com>
2023-04-13 05:55:03 +1000

    first commit

---
+++ MyLib/MyLib.h
@@ -0,0 +1,18 @@
+//
+//  MyLib.h
+//  MyLib
+//
+//  Created by Wen Chen on 2023/4/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLib.
+FOUNDATION_EXPORT double MyLibVersionNumber;
+
+//! Project version string for MyLib.
+FOUNDATION_EXPORT const unsigned char MyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLib/PublicHeader.h>
+
+
---
+++ MyLib/Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Utils.swift
+//  MyLib
+//
+//  Created by Wen Chen on 2023/4/13.
+//
+
+import Foundation
+//import Alamofire
+
+/*
+func getAPICall() {
+    let request = AF.request("https://swapi.dev/api/films")
+    // 2
+    request.responseJSON { (data) in
+      print("============> \(data)")
+    }
+}*/
+
+func show() {
+    print("============>something")
+}
